1b99853d9dd1c198647a62c3ea7e3c764671d934
dreambo8563 <dreambo8563@outlook.com>
2020-06-27 10:26:02 +0800

    feat(api): make it singleton with lazy

--- src/Interface/API/TaskAPI.ts
+++ src/Interface/API/TaskAPI.ts
@@ -4,6 +4,16 @@ export interface ITaskAPI {
 }
 
 export class TaskAPI implements ITaskAPI {
+  //Singleton pattern - Lazy initialization
+  private static instance: TaskAPI | null = null;
+  private constructor() {}
+  static getInstance() {
+    if (null === this.instance) {
+      this.instance = new TaskAPI();
+    }
+
+    return this.instance;
+  }
   async createTask(content: string | URL) {
     const response = await fetch('https://jsonplaceholder.typicode.com/posts', {
       method: 'POST',
--- test/tasklist.test.ts
+++ test/tasklist.test.ts
@@ -4,7 +4,7 @@ import { TaskAPI } from '../src/Interface/API/TaskAPI';
 
 describe('tasklist', () => {
   it('add task', async () => {
-    const api = new TaskAPI();
+    const api = TaskAPI.getInstance();
     const taskService = new TaskUsecase(api);
     const todo = await taskService.createTask('project1- task1');
     console.log(todo);
